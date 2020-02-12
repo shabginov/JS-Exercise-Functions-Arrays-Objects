4c1c8e03202a236d7f5453ff6a4f9ff9b22a951a
Alexey Shabginov <a.shabginov@gmail.com>
2020-02-12 10:37:51 -0800

    Completed two tests about getting cars years and getting only german cars

--- index.js
+++ index.js
@@ -199,8 +199,12 @@ function sortCarInventory(arr) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(arr) {
+  const car_years = [];
+  for (let i = 0; i < arr.length; i++) {
+    car_years.push(arr[i].car_year);
+  }
+  return car_years;
 }
 
 /**
@@ -236,8 +240,14 @@ function getOlderCars(arr, maxYear) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(arr) {
+  const germanCars = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].car_make === 'Audi' || arr[i].car_make === 'Mercedes-Benz' || arr[i].car_make === 'Volkswagen' || arr[i].car_make === 'BMW') {
+      germanCars.push(arr[i]);
+    }
+  }
+  return germanCars;
 }
 
 /**
